6a5971a8e6960bab5c13d12363db60e38e8d4da4
glebe <glebe@BELG04>
2022-09-29 14:53:30 +0200

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information 
+Console.WriteLine("Введите х");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y");
+double y = double.Parse(Console.ReadLine());
+if (y >= -1 && y <= 1 && x >= -1 && x <= 1 && x * x + y * y >= 1) Console.WriteLine(" верно");
+else Console.WriteLine("НЕ верно");
